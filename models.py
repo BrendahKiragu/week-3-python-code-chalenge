13f14a6c1117ec331b55b33f35eba43058929b05
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-09-20 19:28:05 +0300

    establishes object relationships between the models

--- models.py
+++ models.py
@@ -1,15 +1,21 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, create_engine,ForeignKey
 from sqlalchemy.orm import sessionmaker,  relationship
+from sqlalchemy.ext.associationproxy import association_proxy
 
 Base = declarative_base()
 
+#band model
 class Band(Base):
     __tablename__ = 'bands'
 
     id = Column(Integer, primary_key=True)
     name = Column(String())
     hometown = Column(String())
+
+    #relationships
+    concerts = relationship("Concert", back_populates="band")
+    venue = association_proxy('reviews', 'venue', creator=lambda venue: Concert(venue=venue))
 
 #venue model
 class Venue(Base):
@@ -19,6 +25,11 @@ class Venue(Base):
     title = Column(String())
     city = Column(String())
 
+    #relationships
+    concerts = relationship("Concert", back_populates='venue')
+    band = relationship('concerts', 'band', creator=lambda band: Concert(band=band))
+
+#concert model
 class Concert(Base):
     __tablename__ = 'concerts'    
 
@@ -27,6 +38,9 @@ class Concert(Base):
     band_id = Column(Integer(), ForeignKey('bands.id'))#belongs to band
     venue_id = Column(Integer(), ForeignKey('venues.id'))#belongs to venue
 
+    # relationship
+    band =relationship('Band', back_populates='concerts')
+    venue = relationship('Venue', back_populates='concerts')
 
 engine = create_engine('sqlite:///band_concerts.db')    
 
